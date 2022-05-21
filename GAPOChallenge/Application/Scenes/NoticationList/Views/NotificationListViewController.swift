751bbd92be62a11048401ff76103fa786e7024d6
hoangnd17 <dumuc2010@gmail.com>
2022-05-22 05:31:16 +0700

    Update viewcontrollers

---
+++ GAPOChallenge/Application/Scenes/NoticationList/Views/NotificationListViewController.swift
@@ -0,0 +1,52 @@
+//
+//  NotificationListViewController.swift
+//  GAPOChallenge
+//
+//  Created by Nguyen Dinh Hoang on 5/19/22.
+//
+
+import UIKit
+
+class NotificationListViewController: UIViewController {
+    
+    @IBOutlet private weak var tableView: UITableView!
+    @IBOutlet private weak var searchBarContainer: UIView!
+    
+    private let searchController = UISearchController(searchResultsController: nil)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+    }
+
+    private func setupViews() {
+        setupSearchController()
+    }
+
+}
+
+extension NotificationListViewController {
+    private func setupSearchController() {
+        searchController.delegate = self
+        searchController.searchBar.delegate = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.translatesAutoresizingMaskIntoConstraints = true
+        searchController.searchBar.barStyle = .default
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchBar.frame = searchBarContainer.bounds
+        searchController.searchBar.autoresizingMask = [.flexibleWidth]
+        searchBarContainer.addSubview(searchController.searchBar)
+        definesPresentationContext = true
+    }
+}
+
+extension NotificationListViewController: UISearchBarDelegate {
+    
+}
+
+extension NotificationListViewController: UISearchControllerDelegate {
+    
+}
+
+
